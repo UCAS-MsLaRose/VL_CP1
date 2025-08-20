061acc6ebd29ddb232b28897b49f23c807ec00af
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-08-20 10:28:44 -0600

    inputs notes

---
+++ notes_1st/inputs.py
@@ -0,0 +1,19 @@
+# VL 1st Inputs and Outputs Notes
+
+#5 What is an input
+#A way for the code to get information 
+
+#6 What is the purpose of inputs
+#Code isn't helpful if it doesn't use new information
+name = input("What is your name: ")
+# ^      ^             ^
+# Save the input where it can be accessed again
+# Use keyword "input"
+# Inside parens give user instructions
+
+print("What is your name?")
+name = input()
+
+#Both instruction methods work, but you have to do it the same way all the time in your code
+
+print("Hello",name)
---
+++ notes_2nd/inputs.py
@@ -0,0 +1,13 @@
+# VL 2nd Inputs and outputs notes
+
+#5 Input gives information to the program
+#6 Inputs allow the code to change because it is working with new informaiton
+color = input("Give me a color: ")
+print("Your color is", color)
+
+# Other option
+print("Tell me a color: ")
+color_two = input()
+print(color_two)
+
+#Pick 1 method, do not change between them
