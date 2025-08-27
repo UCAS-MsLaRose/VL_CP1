eb9d13d8dbb06b5549cf1bec9af5a2cab8c3e42a
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-08-27 09:52:49 -0600

    Number notes

---
+++ notes_1st/numbers.py
@@ -0,0 +1,41 @@
+# VL 1st Integers and floats notes
+
+#Integers
+age = 999999999999999999999
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+#Float (Floating point number)
+pi = 3.14
+percent = -.75
+damage = 9.75
+
+#MATH
+print(5.0+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5 
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print(5+2 *9 + (5**5)%3)
+
+#Type casting
+num = int(3.14) #Does not round! 
+decimal = float(8)
+print(num)
+print(decimal)
+
+#Useing Variables
+hours = 3
+minutes = 4
+
+print(hours*60+minutes)
+
+#To change a user input data type you have to surround the ENTIRE input! 
+age = int(input("How old are you: \n"))
+
+print("Wow you are are old that is", 525600*age, "minutes!")
