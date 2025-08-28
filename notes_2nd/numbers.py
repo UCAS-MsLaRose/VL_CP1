898296d0530082877ef63dcbcfbec3a2ec054411
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-08-28 10:41:24 -0600

    notes

--- notes_2nd/numbers.py
+++ notes_2nd/numbers.py
@@ -14,3 +14,16 @@ patience_level = -.50
 
 #MATH!!!
 print(8/4 +7-(8+2)%4**2)
+
+#Change data types
+age = int(input("How old are you?\n"))
+
+print("Wow that is old! That is", 525600*age, "minutes!")
+
+brain_cells = float(input("How many brain cells does your class have? (Example: .3)\n"))
+
+print("Are you sure it isn't", brain_cells/2, "?")
+
+#Round
+pi = 3.14159265358979
+print(round(pi, 4)) # <= difficult key word round. Needs 2 things 1. What number needs rounded 2. How many decimals do you want
