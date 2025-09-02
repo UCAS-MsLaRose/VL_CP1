b74a6724d5f551526c3860290812f8772bdafdcf
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-02 10:51:16 -0600

    Strings notes

--- notes_2nd/strings.py
+++ notes_2nd/strings.py
@@ -1,2 +1,38 @@
 # VL 2nd Strings Notes
 
+# What is a string
+
+
+# Examples
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = 'Xavier'
+month = 'September'
+food = "chocolate"
+last_name = "LaRose"
+
+# If I need to use ' or "
+name = "La'Rose"
+best = '"The Return of the King" is the best book ever!'
+
+funny = "The dog jumped into the lake" 
+
+
+# Length of strings
+length = len(month)
+print("September has", length, "letters")
+
+
+# Escape Character 
+print('\tWill said: \n\t\t\t\t\t"Don\'t die"')
+
+
+# Concatenate
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# Index
+
+
+# Slice
+print(last_name[2:6])
