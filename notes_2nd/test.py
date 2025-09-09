3d82a3fcec815d4870327e15453773e96cd624ea
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-09 16:26:48 +0000

    Added print to test.py

--- notes_2nd/test.py
+++ notes_2nd/test.py
@@ -1,4 +1,5 @@
 print("Test")
 print("Update")
 print("Practice sync and homework")
+print("I added this on codespaces. That place where I can do homework!")
 #print() <= Everything inside of the parentheses is going to be printed
