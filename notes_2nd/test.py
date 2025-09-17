f996e474ebf6b3b68a69863a7e15e9caf1dcb7db
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-17 10:45:43 -0600

    Got a new seat

--- notes_2nd/test.py
+++ notes_2nd/test.py
@@ -2,4 +2,5 @@ print("Test")
 print("Update")
 print("Practice sync and homework")
 print("I added this on codespaces. That place where I can do homework!")
+print("New seat 9/17")
 #print() <= Everything inside of the parentheses is going to be printed
