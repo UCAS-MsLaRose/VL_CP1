f756da4808715e258d60eab7491c14c0aac41627
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-09-25 13:58:19 -0600

    Notes

---
+++ notes_1st/debugging.py
@@ -0,0 +1,16 @@
+# VL 1st DEBUGGING NOTES! 
+
+# Syntax Error - grammar error
+    #print"Vienna"
+# => Read error message, go to that line of code, fix the syntax
+
+# Logic Errors - We gave the wrong steps
+numone = "2"
+numtwo = "5"
+
+print(numone + numtwo)
+
+# Have well thought out plan, Step by step go through our logic 
+
+# Run-time Error
+
---
+++ notes_2nd/debugging.py
@@ -0,0 +1,13 @@
+# VL 2nd Debugging Notes
+
+# Syntax Error - Grammar error
+    #print"Ms. LaRose"
+# Fix by => reading where the problem is AND fixing it!
+
+# Logic Error - The computer has the wrong steps
+num = "10"
+num_two = "3"
+
+print(num+num_two)
+
+# Run-Time Errors
