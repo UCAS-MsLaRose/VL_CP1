8ff8e47fe0316e8967fea559fac7ea3785f36008
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-10-01 10:45:31 -0600

    For loops notes

---
+++ notes_2nd/for_loop.py
@@ -0,0 +1,28 @@
+# VL 2nd For Loops
+import time
+
+nums = [6,51,61,94,351,946,5489,4,654,684]
+
+for num in nums:
+    div = num/2
+    if div > 100:
+        print(f"{div} is half of {num}. And it is still a large number!")
+    else:
+        print(num)
+
+print("We completed all the numbers!")
+time.sleep(3)
+
+for x in range(1,10):
+    print(x)
+time.sleep(3)
+
+print("Count by twos")
+for x in range(2,11,2):
+    print(x)
+time.sleep(3)
+
+print("Count down")
+for x in range(10,0, -1):
+    print(x)
+    time.sleep(1)
