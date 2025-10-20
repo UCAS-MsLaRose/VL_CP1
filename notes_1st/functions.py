2771e30dd34e3832c62e3c7f6fc47ec8ec30d8a0
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-10-20 10:51:20 -0600

    functions notes

--- notes_1st/functions.py
+++ notes_1st/functions.py
@@ -1,1 +1,25 @@
 #VL, 1st period Functions Notes
+#Set variables
+
+#define functions 
+def add(x,y):
+    return x+y
+
+def initials(name):
+    names = name.split(" ")
+    initials = ""
+    for name in names:
+        initials += name[0]
+
+    return initials
+
+total = add(5,5)
+print(total)
+print(f"10 + 72 = {add(10,72)}")
+x = 0
+while x < add(3,9):
+    print("duck")
+    x+= 1
+
+print("Goose!")
+
---
+++ notes_2nd/functions.py
@@ -0,0 +1,13 @@
+# VL, 2nd Period, Functions Notes
+num = 0
+def add(x,y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num += 1
+print("Goose")
+print(f"Results is: {add(-6519146,36516)}")
+total = add(689461,654894)
+print(add(add(3.14,.85), 10))
+add(42,7)
