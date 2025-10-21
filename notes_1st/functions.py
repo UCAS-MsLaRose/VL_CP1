86293dccceb79626c18ee428d2ddf9835de1ba82
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-10-21 10:53:17 -0600

    functions notes finished

--- notes_1st/functions.py
+++ notes_1st/functions.py
@@ -1,7 +1,18 @@
 #VL, 1st period Functions Notes
 #Set variables
+player_health = 100
+monster_health = 100
 
 #define functions 
+def damage(amount, turn):
+    if turn == "player":
+        return monster_health - amount, player_health
+    else:
+        return monster_health, player_health - amount
+
+monster_health, player_health = damage(10, "player")
+print(f"Monster Health: {monster_health}")
+print(f"Player Health: {player_health}")
 def add(x,y):
     return x+y
 
@@ -22,4 +33,8 @@ while x < add(3,9):
     x+= 1
 
 print("Goose!")
+print(initials("Tia LaRose"))
+print(initials("Franco Barboza"))
 
+print(f"a = {ord("a")}")
+print(f"98 = {chr(98)}")
--- notes_2nd/functions.py
+++ notes_2nd/functions.py
@@ -1,8 +1,29 @@
 # VL, 2nd Period, Functions Notes
+#all imports 
+# set global variables
 num = 0
+player_hp = 100
+monster_hp = 100
+
+# write your functions
 def add(x,y):
     return x+y
 
+def initials(name):
+    names = name.split(" ")
+    initial = ""
+    for name in names:
+        initial += name[0]
+    return initial
+
+def attack(dmg, turn):
+    if turn == "player":
+        return monster_hp - dmg, player_hp
+    else:
+        return monster_hp, player_hp - dmg
+
+
+# Write the rest of the code
 while num < add(5,5):
     print("Duck")
     num += 1
@@ -11,3 +32,18 @@ print(f"Results is: {add(-6519146,36516)}")
 total = add(689461,654894)
 print(add(add(3.14,.85), 10))
 add(42,7)
+
+
+
+print(f"Tia's initials are: {initials("Tia LaRose")}")
+print(f"Xavier's initials are: {initials("Xavier LaRose")}")
+monster_hp, player_hp = attack(15, "monster")
+print(f"Player Health: {player_hp}")
+print(f"Monster Health: {monster_hp}")
+
+monster_hp, player_hp = attack(15, "player")
+print(f"Player Health: {player_hp}")
+print(f"Monster Health: {monster_hp}")
+
+print(f"a = {ord("a")}")
+print(f"100 = {chr(100)}")
